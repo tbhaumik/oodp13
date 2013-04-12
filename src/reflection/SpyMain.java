9a5dc9c5780e5dafd642179afb00a5d81665bb49
Keith Mannock <keith@dcs.bbk.ac.uk>
2013-04-12 23:01:17 +0100

    reflection example

---
+++ src/reflection/SpyMain.java
@@ -0,0 +1,12 @@
+package reflection;
+
+public class SpyMain {
+    public static void main(String args[]) {
+        try {
+            Spy thisClassPrinter = Spy.getSpy("Spy");
+            thisClassPrinter.printAll(args);
+        } catch (ClassNotFoundException ex) {
+            System.err.println("It broke!");
+        }
+    }
+}
